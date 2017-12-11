dece66d0438a7037b625218e7416a823edbfe3f0
Faen <evgeniy.arefa@i.ua>
2017-12-11 15:56:19 +0200

    Version 1.07

--- agreement/tests/tests_models.py
+++ agreement/tests/tests_models.py
@@ -5,36 +5,20 @@ from django.contrib.auth.models import User
 
 class ModelTest(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        user = User.objects.create(
-            password='qwertyuiop', username='a', first_name='A',
-            last_name='a', email='a@.net', id=1
-            )
-        neg = Negotiator.objects.create(name=user)
-        country = Country.objects.create(name='Zambia', code='ZMB', id=1)
-        company = Company.objects.create(name='Kukh', country=country, id=1)
-        agreement = Agreement.objects.create(
-            data_start='2017-01-01', data_end='2017-12-31', company=company,
-            negotiator=neg, loan_turnover=0, debit_turnover=0, id=1
-            )
-        Period.objects.create(
-            data_start='2017-02-01', data_end='2017-03-01', status=1,
-            agreement=agreement, id=1
-            )
+    fixtures = ['D:/Python/Projects/Django/Work/B2B_2/fixtures/data.json']
 
     def test_neg__str__(self):
-        neg = Negotiator.objects.get()
-        self.assertEquals(neg.__str__(), 'a')
+        neg = Negotiator.objects.get(id=1)
+        self.assertEquals(neg.__str__(), 'user_a')
 
     def test_country__str__(self):
-        country = Country.objects.get()
-        self.assertEquals(country.__str__(), 'Zambia')
+        country = Country.objects.get(id=1)
+        self.assertEquals(country.__str__(), 'Australia')
 
     def test_company__str__(self):
-        company = Company.objects.get()
-        self.assertEquals(company.__str__(), 'Kukh')
+        company = Company.objects.get(id=1)
+        self.assertEquals(company.__str__(), 'AusAirLine')
 
     def test_period__str__(self):
-        period = Period.objects.get()
-        self.assertEquals(period.__str__(), '2017-02-01/2017-03-01/1')
+        period = Period.objects.get(id=1)
+        self.assertEquals(period.__str__(), '2017-01-29/2017-11-29/1')
--- agreement/tests/tests_views.py
+++ agreement/tests/tests_views.py
@@ -6,23 +6,7 @@ from django.core.urlresolvers import reverse
 
 class MainTests(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        user = User.objects.create(
-            password='qwertyuiop', username='a', first_name='A',
-            last_name='a', email='a@.net', id=1
-            )
-        neg = Negotiator.objects.create(name=user)
-        country = Country.objects.create(name='Zambia', code='ZMB', id=1)
-        company = Company.objects.create(name='Kukh', country=country, id=1)
-        agreement = Agreement.objects.create(
-            data_start='2017-01-01', data_end='2017-12-31', company=company,
-            negotiator=neg, loan_turnover=0, debit_turnover=0, id=1
-            )
-        Period.objects.create(
-            data_start='2017-02-01', data_end='2017-03-01',
-            status=1, agreement=agreement, id=1
-            )
+    fixtures = ['D:/Python/Projects/Django/Work/B2B_2/fixtures/data.json']
 
     def test_calendar_url_exists(self):
         request = self.client.get('/agreements/calendar/')
